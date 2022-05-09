01878f5143644b7893b7b6d3ad4fb2fc3e4078e6
Mahesh Vagicherla <maheshvagicherla99438@gmail.com>
2022-05-10 00:18:25 +0530

    add routing page and nextui

---
+++ pages/onboarding.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import React from "react";
+function onboarding() {
+  return (
+    <>
+      <Head>
+        <title>Onboarding</title>
+      </Head>
+      <div>onboarding</div>
+    </>
+  );
+}
+
+export default onboarding;
